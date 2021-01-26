c590fb1c303c4cf139a1dff03b4b809a67432d05
Anmol Rai <raianmol235@gmail.com>
2021-01-26 18:46:39 +0530

    add recursive method too

--- length of the list.py
+++ length of the list.py
@@ -8,13 +8,18 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def countNode(self):
+    def length_Iterative(self):
         temp = self.head
         count = 0
         while temp is not None:
             count = count + 1  # we can also write it like count += 1
             temp = temp.next
         return count
+
+    def length_Recursive(self, node):
+        if node is None:
+            return 0
+        return 1 + self.length_Recursive(node.next)
 
     def printList(self):
         temp = self.head
@@ -32,4 +37,5 @@ l1.head.next = l2
 l2.next = l3
 l3.next = l4
 l1.printList()
-print("the length of the linked list is :", l1.countNode())
+print("the length of the linked list is :", l1.length_Iterative())
+print("the length of the linked list is :", l1.length_Recursive(l1.head))
