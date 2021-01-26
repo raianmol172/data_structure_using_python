d4772beacf42d8f1bc2c65bc42c1295d92b1d045
Anmol Rai <raianmol235@gmail.com>
2021-01-26 19:09:57 +0530

    append and prepend

--- create_linked_list.py
+++ create_linked_list.py
@@ -1,4 +1,4 @@
-# we had created a node here which has data and a pointer which points towards none for now
+# we had created a node
 class Node:
     def __init__(self, data):
         self.data = data
@@ -14,19 +14,33 @@ class Linked_list:
 
     def printList(self):  # the function that will print the list
         temp = self.head
-        while temp.next is not None:
+        while temp:
             print(temp.data)
             temp = temp.next
 
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
 
-# we have created the list here
+    def prepend(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+
 l1 = Linked_list()
-l1.head = Node(100)
-l2 = Node(200)
-l3 = Node(300)
-l4 = Node(400)
-# we have link the list with each other
-l1.head.next = l2
-l2.next = l3
-l3.next = l4
+l1.append(100)
+l1.append(200)
+l1.prepend(10)
+l1.prepend(20)
 l1.printList()
