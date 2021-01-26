d8c4c145d238489076286a7d7e6dcaafbf764426
Anmol Rai <raianmol235@gmail.com>
2021-01-26 17:53:04 +0530

    changes values

--- delete_data_in_list.py
+++ delete_data_in_list.py
@@ -34,11 +34,11 @@ class LinkedList:
         temp = None
 
 l1 = LinkedList()
-l1.head = Node('Anmol')
-l2 = Node('Manish')
-l3 = Node('Abhishek')
-l4 = Node('Aayush')
-l5 = Node('chimmo')
+l1.head = Node('A')
+l2 = Node('B')
+l3 = Node('C')
+l4 = Node('D')
+l5 = Node('E')
 l1.head.next = l2
 l2.next = l3
 l3.next = l4
@@ -46,7 +46,7 @@ l4.next = l5
 print("before deletion: ")
 l1.printList()
 print("after deletion")
-l1.deleteNode('Anmol')
-l1.deleteNode('Aayush')
-l1.deleteNode('karan')
+l1.deleteNode('A')
+l1.deleteNode('C')
+l1.deleteNode('E')
 l1.printList()
