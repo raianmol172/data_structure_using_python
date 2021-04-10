9f43d0b42b3fe5e44a337a870d88b25a9545ad42
Anmol Rai <raianmol235@gmail.com>
2021-04-10 20:08:14 +0530

    construct binary tree from preorder and inorder

---
+++ construct_binaryTree_preorder_inorder.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def preorder(self, start):
+        if start is None:
+            return
+
+        print(start.data)
+        self.preorder(start.left)
+        self.preorder(start.right)
+
+
+    def constructBinaryTree(self, preorder: list[int], inorder: list[int]):
+        if len(inorder) == 0:
+            return
+        if len(preorder) == 1:
+            return Node(preorder[0])
+        idx = inorder.index(preorder.pop(0))  # pop element from preorder and save the index of it in inorder.
+        root = Node(inorder[idx])  # created a node of popped element from inorder list
+
+        root.left = self.constructBinaryTree(preorder, inorder[:idx])
+        root.right = self.constructBinaryTree(preorder, inorder[idx + 1:])
+
+        return root
+
+
+preorder = [1, 2, 4, 8, 9, 5, 10, 11, 3, 6, 7]
+inorder = [8, 4, 9, 2, 10, 5, 11, 1, 6, 3, 7]
+tree = Solution()
+root = tree.constructBinaryTree(preorder, inorder)
+tree.preorder(root)
